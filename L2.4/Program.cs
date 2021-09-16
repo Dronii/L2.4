e4167f073850c1605d6a70d3e4143c82e128bfe9
Dronii <redtiberium86@gmail.com>
2021-09-16 16:12:54 +0200

    Добавьте файлы проекта.

---
+++ L2.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace L2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a;
+            a = Convert.ToInt32(Console.ReadLine());
+
+            if (a % 3 == 0)
+                Console.WriteLine($"{a} кратно 3");
+             else if (a % 2 == 0) 
+                Console.WriteLine($"{a} кратно 2 ");
+            else
+                Console.WriteLine($"{a} NE кратно 2 или 3");
+            if ((a % 10 == 0) & (a % 3 != 0))
+                Console.WriteLine($"{a} true ");
+            else
+                Console.WriteLine($"{a} false");
+            Console.WriteLine(); 
+        }
+    }
+}
